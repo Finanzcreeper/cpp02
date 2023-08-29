4bc18aac724f0c8143961fe0e3171a622d7c23b3
Nico Reher <nreher@student.42berlin.de>
2023-08-29 10:57:56 +0200

    ex01 added

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,47 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() : fixed (0)
+{
+	std::cout << "A Fixed number got spawned." << std::endl;
+}
+
+Fixed::Fixed(const int input) : fixed (input << fractional_bits)
+{
+	std::cout << "A Fixed number got spawned from an int" << std::endl;
+}
+
+Fixed::Fixed(const float input) : fixed (roundf (input * (1 << fractional_bits)))
+{
+	std::cout << "A Fixed number got spawned from an float" << std::endl;
+}
+
+Fixed& Fixed::operator=(const Fixed& source)
+{
+	std::cout << "Mitosis complete." << std::endl;
+	fixed = source.fixed;
+	return (*this);
+}
+
+Fixed::Fixed(Fixed &source)
+{
+	std::cout << "A Fixed number got spawned." << std::endl;
+	*this = source;
+	fixed = source.fixed;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "the deconstructor Wrecked a class." << std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits got a " << fixed << ", just for you." << std::endl;
+	return(fixed);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	std::cout << "I put "<< raw << " into the class for you." << std::endl;
+	fixed = raw;
+}
---
+++ ex01/Fixed.hpp
@@ -0,0 +1,23 @@
+#ifndef FIXED_H
+#define FIXED_H
+
+#include <string>
+#include <iostream>
+#include <cmath>
+class Fixed
+{
+private:
+	int	fixed;
+	static const int	fractional_bits = 8;
+public:
+	Fixed();
+	Fixed(const int input);
+	Fixed(const float input);
+	Fixed(Fixed &source);
+	Fixed& operator=(const Fixed& source);
+	~Fixed();
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+};
+
+#endif
